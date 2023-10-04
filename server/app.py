ba3e628f4429171721f8090acae0088d7c528c9d
Curtisodell1 <46852021+Curtisodell1@users.noreply.github.com>
2023-10-04 16:52:07 -0600

    second first push

--- server/app.py
+++ server/app.py
@@ -3,13 +3,17 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, request, session
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask import Flask, request, make_response
 
 # Local imports
 from config import app, db, api
 # Add your model imports
 
+from models import User, Task, TaskContainer, Entry, TaskAssignment
 
 # Views go here!
 
--- server/models.py
+++ server/models.py
@@ -1,6 +1,23 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
-# Models go here!
+class User():
+    pass
+    
+class Task():
+    pass
+    
+class TaskContainer():
+    pass
+
+class Entry():
+    pass
+
+class TaskAssignment():
+    pass
